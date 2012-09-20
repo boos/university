f65ac6a6bd84de218a2cdde3443bf1959b9ef2d2
Roberto Martelloni <boos@core-dumped.info>
2012-09-20 18:16:36 +0200

    added main file and csv load data ..

---
+++ softcomputing/main.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python
+
+import csv
+
+import geometric_transformation
+
+START_POSITION_FILE = "data/start_position"
+CAMERAS_DATA_FILE   = "data/camera_rotations"
+
+
+def main():
+
+    point = geometric_transformation.geometric_transformation(3,5,7)
+
+    with open(CAMERAS_DATA_FILE, 'rb') as csvfile:
+        data = csv.reader(csvfile, skipinitialspace=True)
+
+        for row in data:
+
+            if len(row) == 0: continue
+            if row[0][0] == '#': continue
+
+            point.rotate_X_axis(row[0]) # rotate of value
+
+            print "actual_position:", point.actual_position
+
+            point.rotate_X_axis(row[1]) # rotate as the error value 
+
+            point.rotate_Y_axis(row[2]) # rotate on Y as value 
+            point.rotate_Y_axis(row[3]) # rotate as the error value  
+
+            point.rotate_Z_axis(row[4]) # rotate on Y as value 
+            point.rotate_Z_axis(row[5]) # rotate as the error value 
+
+            point.traslate(row[6], row[7], row[8])
+
+
+
+
+
+
+
+
+
+
+
+__init__ = main()
