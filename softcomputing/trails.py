861b2d98fbf3e73dc744451c7ce27b56a8de3e96
Roberto Martelloni <boos@core-dumped.info>
2013-04-24 16:26:26 +0200

    udpate trails methods with real actions from slides and wikipedia .

--- softcomputing/trails.py
+++ softcomputing/trails.py
@@ -1,20 +1,70 @@
 import thread
 
 class trails:
+    """ Trails are updated at each interation of the alghorithm, 
+        increasing the level of those related to moves that were part of 'good' solutions, 
+        while decreasing all others. 
 
-	# initialize a 3D matrix with all the feasible moves 
-	def __init__(s):
+        After each iteration t of the algorithm, trails are updated
+        using the following formula.
 
+        τ ιψ (t ) = ρτιψ (t − 1) + ∆τ ιψ
+
+        where ρ is a user-defined evaporation coefficient and
+        ∆τιψ represents the sum of the contributions of all ants
+        that used move (ιψ) to construct their solution.
+
+        As iteration I mean after each set of ANTs have computed a solution.
+        So at every algorithms interation first of all decrease all the value on the TRAILS. 
+        Then for every ANT, increase the trails 
+        For every ANT:
+        - decrease first of all all the value of the TRAILS and th
+
+        """
+
+	# initialize a 3D matrix with all the moves of previous ANTs 
+	def __init__(s, evaporation_coefficient, pherormone_constant):
+
+        # create ADT for memorize trails 
 		s.trails = dict()
+
+        # Store evaporation coefficient of the pherormone 
+        s.evaporation_coefficient = evaporation_coefficient
+
+        # Store of how much every ANT increase pherormone trails when use a path 
+        s.pherormone_constant = pherormone_constant 
+
+        # create lock variable to handle access to critical sections 
 		s.lock = thread.allocate_lock()
 
-	def update(s, selected_move):
-		""" update trails used by ant """
-		# TODO: fix trails update using the slides
 
-		x = selected_move[0]
-		y = selected_move[1]
-		z = selected_move[2]
+    def evaporation_action(s):
+        """ Decrease globally all the trails """
+
+		# start of critical section 
+		s.lock.acquire()
+
+        for x in trails:
+            for y in trails[x]:
+                for z in trails[x][y]:
+                    trails[x][y][z] = s.evaporation_coefficient * trails[x][y][z] 
+
+		# end of critical section
+		s.lock.release()
+
+    
+	def update(s, selected_ant_moves):
+		""" Increase trails used by single ANT 
+            
+            For every moves the ANT have done increase the trails on that path 
+            Trails are increase with value of Q/L . 
+            Q is a constant, L is the length of the complete solution """
+
+        for selected_move in selected_ant_moves:
+
+		    x = selected_move[0]
+    		y = selected_move[1]
+    		z = selected_move[2]
 
 		# start of critical section 
 		s.lock.acquire()
@@ -28,7 +78,8 @@ class trails:
 		if not s.trails[x][y].has_key(z):
 			s.trails[x][y][z] = 0
 
-		s.trails[x][y][z] = s.trails[x][y][z] + 1
+        
+		s.trails[x][y][z] = s.pherormone_constant / len (selected_ant_moves)
 
 		s.lock.release()
 		# end of critical section
@@ -53,8 +104,3 @@ class trails:
 		# end of critical secion 
 
 		return value
-
-	def evaporate(s):
-		# TODO 
-		# did I need this functionality ?
-
